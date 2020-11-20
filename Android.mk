3d9a272ebb33fa6ce765c355ef87bb7800a61653
g <gWOLF3@protonmail.com>
2020-11-20 16:08:29 -0600

    init

---
+++ Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := vpn
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true 
+LOCAL_MODULE_CLASS := APPS
+LOCAL_SRC_FILES := vpn.apk
+include $(BUILD_PREBUILT)
