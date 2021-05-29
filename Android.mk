66dc2bc6d128b87029e3072c2a51d8cf420dd695
g <gWOLF3@protonmail.com>
2021-05-29 14:47:56 -0500

    mobamba

--- Android.mk
+++ Android.mk
@@ -1,9 +1,9 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := VPN
+LOCAL_MODULE := Wireguard
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_CLASS := APPS
-LOCAL_SRC_FILES := vpn.apk
+LOCAL_SRC_FILES := wireguard.apk
 include $(BUILD_PREBUILT)
